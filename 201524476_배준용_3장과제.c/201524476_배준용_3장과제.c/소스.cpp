84725acc9f999a4acf975c818ba65e74163c856e
ertttyu <43158576+ertttyu@users.noreply.github.com>
2018-09-28 13:29:55 +0900

    201524476 배준용
    과제제출

---
+++ 201524476_배준용_3장과제.c/201524476_배준용_3장과제.c/소스.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+	float salary;
+	int imate = 2;
+	long shot = 53456;
+	char grade = 'A';
+	float log = 2.71828;	
+	printf("The odds against the %d were %d to 1.\n", imate, shot);
+	printf("A score of %f is not an %c grade.\n", log, grade);  
+
+	system("pause");
+
+	return 0;
+}
